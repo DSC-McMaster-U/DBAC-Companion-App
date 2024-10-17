46ca757c384084c15113fc250c1e84ebbc210ab1
ericpark11 <122037200+ericpark11@users.noreply.github.com>
2024-10-17 13:45:13 -0400

    npm ERR! Missing script: "dev"

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,48 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
+// const cors = require('cors');
+const pool = require('./db')
+const port = `1337`
 
+const app = express();
 // Middleware to enable CORS
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
-// Example route
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
+// Route
+app.get('/', async (req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM user')
+        res.status(200).send( data.rows )
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 });
 
+app.post('/', async (req, res) => {
+    const { name, location } = req.body
+    try {
+        await pool.query('INSERT INTO user (first_name, last_name, mac_id, student_num, is_fulltime) VALUE ($1, $2, $3, $4, $5)', [first_name, last_name, mac_id, student_num, is_fulltime])
+        res.status(200).send({message: "Successfully added user" })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+app.get('/setup', async (req, res) => {
+    try {
+        await pool.query('CREATE TABLE user( id SERIAL PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), mac_id VARCHAR(50), student_num CHAR(9), is_fulltime BOOLEAN')
+        res.status(200).send({message: "Successfully created table" })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// const PORT = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
