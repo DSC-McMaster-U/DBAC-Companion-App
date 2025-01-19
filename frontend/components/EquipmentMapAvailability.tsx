ee5c2dd4e4206ee89bdb807977563be3f3f6314b
Abdallah Alqashqish <abdalq2003@gmail.com>
2025-01-18 23:06:04 -0500

    Fix EquipmentMapAvailability component to be compatible with map SVG UI

--- frontend/components/EquipmentMapAvailability.tsx
+++ frontend/components/EquipmentMapAvailability.tsx
@@ -1,17 +1,26 @@
-import { Image, View, ViewProps, StyleSheet} from "react-native";
+import { StyleSheet} from "react-native";
+import { Rect, Image, RectProps } from "react-native-svg";
 
-type EquipmentMapAvailability = ViewProps & {
+type EquipmentMapAvailability = RectProps & {
     equipmentImage: any,
     availabilityColor: string
 }
 
 // e.g. <EquipmentMapAvailability equipmentImage={require('@/assets/images/bicepcurl-machine.png')} availabilityColor="red" />
-export default function EquipmentMapAvailability({equipmentImage, availabilityColor}: EquipmentMapAvailability) {
+export default function EquipmentMapAvailability({
+    x,
+    y,
+    equipmentImage, 
+    availabilityColor,
+    ...rest
+}: EquipmentMapAvailability) {
     return (
-    <View style={styles.icon}>
-        <Image style={{width: 25, height: 25, objectFit: 'contain'}} source={equipmentImage} />
-        <View style={[styles.availability, {backgroundColor: availabilityColor ?? "red"}]}></View>
-    </View>)
+        <>
+            <Rect x={x} y={y} width={22} height={13} fill={'grey'} rx={5} ry={5} {...rest} />
+            <Image x={x+1} y={y+1} width={11} height={11} rx={11} ry={11} href={equipmentImage} />
+            <Rect x={x+13} y={y+3} width={7} height={7} rx={12} ry={12} fill={availabilityColor ?? "red"}></Rect>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
