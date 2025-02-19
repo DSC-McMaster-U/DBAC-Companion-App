7847fc94898361e85fa6251a3c71d7851f88436b
Abdallah Alqashqish <abdalq2003@gmail.com>
2025-02-19 03:02:52 -0500

    Implement collecting user first and last name on email/password registration

--- frontend/app/signup.tsx
+++ frontend/app/signup.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import { View, Text, SafeAreaView, TextInput, Button, StyleSheet } from "react-native";
 import { auth } from "@/FirebaseConfig";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 export default function signup() {
     const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [password, setPassword] = useState('');
 
     const signUpEmailPassword = async () => {
         try {
-            await createUserWithEmailAndPassword(auth, email, password);
+            // Register the user
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+
+            // Update the users profile with their display name (First name Last Name)
+            await updateProfile(user, {
+                displayName: `${firstName} ${lastName}`
+            });
         } catch(error: any) {
             console.log(error);
             alert("Registration Failed: " + error.message);
@@ -23,6 +32,22 @@ export default function signup() {
                     <Text style={styles.signUpText}>Sign Up</Text>
                 </View>
                 <View style={{ width: "100%", gap: "10%" }}>
+                    <TextInput 
+                        value={firstName}
+                        onChangeText={setFirstName}
+                        placeholder={"First Name"}
+                        placeholderTextColor="black"
+                        style={{ borderWidth: 1, fontSize: 20 }}
+                    />
+
+                    <TextInput 
+                        value={lastName}
+                        onChangeText={setLastName}
+                        placeholder={"Last Name"}
+                        placeholderTextColor="black"
+                        style={{ borderWidth: 1, fontSize: 20 }}
+                    />
+                    
                     <TextInput 
                         value={email}
                         onChangeText={setEmail}
