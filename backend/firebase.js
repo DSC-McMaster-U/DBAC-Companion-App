fd3c309381987dbd7f5e054c43821e28105374eb
Abdallah Alqashqish <abdalq2003@gmail.com>
2025-03-04 21:05:43 -0500

    Use firebase admin SDK for backend instead of client SDK

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,27 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, setDoc, getDocs, doc, updateDoc } from "firebase/firestore";
+import admin from "firebase-admin";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDvLWO-U2B4JCBx0MIeaRQKtAPUqvCM5TQ",
-  authDomain: "dbac-app.firebaseapp.com",
-  projectId: "dbac-app",
-  storageBucket: "dbac-app.appspot.com",
-  messagingSenderId: "995867460373",
-  appId: "1:995867460373:web:90cda938c9b11e1380df1c",
-  measurementId: "G-QKRQCDYJBK"
-};
+admin.initializeApp({
+  credential: admin.credential.cert("./serviceAccountKey.json")
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+const db = admin.firestore();
+const auth = admin.auth();
 
 // Function to add a document with a custom ID
 const addDocumentWithId = async (collectionName, docId, documentData) => {
   try {
-    await setDoc(doc(db, collectionName, docId), documentData);
+    const collectionRef = db.collection(collectionName);
+    await collectionRef.doc(docId).set(documentData);
     console.log("Document written with ID: ", docId);
     return docId; // Return the custom document ID
   } catch (error) {
@@ -31,12 +21,14 @@ const addDocumentWithId = async (collectionName, docId, documentData) => {
 };
 
 const updateDocument = async (collectionName, docId, data) => {
-  const docRef = doc(db, collectionName, docId);
-  await updateDoc(docRef, data);
+  const docRef =  db.collection(collectionName).doc(docId);
+  await  docRef.update(data);
 };
+
 const getAllDocuments = async (collectionName) => {
-  const querySnapshot = await getDocs(collection(db, collectionName));
+  const collectionRef = db.collection(collectionName);
+  const querySnapshot = await collectionRef.get();
   return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
-export { db, addDocumentWithId, getAllDocuments, updateDocument };
+export { db, auth, addDocumentWithId, getAllDocuments, updateDocument };
