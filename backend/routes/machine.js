265744d386340f7a111f47899455b81c48093f27
Abdallah Alqashqish <abdalq2003@gmail.com>
2025-03-12 15:06:46 -0400

    Remove old endpoints

--- backend/routes/machine.js
+++ backend/routes/machine.js
@@ -3,10 +3,6 @@ import express from "express";
 import {
     getMachineInfo,
     getMachines,
-    updateMachineUser,
-    updateSetsLeft,
-    updateQueue,
-    updateWorkIn,
     useMachine
 } from "../controllers/machine.js";
 
@@ -16,9 +12,5 @@ const router = express.Router()
 router.get("/machines", getMachines)
 router.post("/machines/machine", getMachineInfo)
 router.post("/machines/use", useMachine);
-router.patch("/machines/users", updateMachineUser)
-router.patch("/machines/setsleft", updateSetsLeft)
-router.patch("/machines/queue", updateQueue)
-router.patch("/machines/workin", updateWorkIn)
 
 export default router
